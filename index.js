30783b52143558fbeab8abf526c8ab16c4169462
CodeWithGoyal <hgoyal5555614@gmail.com>
2023-03-24 14:25:44 +0530

    v1.1

--- index.js
+++ index.js
@@ -3,26 +3,27 @@ import express from "express";
 import { createServer } from "http";
 import Connection from "./database/db.js";
 import { getDocument, updateDocument } from "./controller/documentController.js";
+import * as path from "path"
 
 const PORT = process.env.PORT || 9000;
 const MONGODB_URL = process.env.MONGODB_URL;
 
 await Connection(MONGODB_URL);
 
-console.log("server is running on port ", PORT);
-
 const app = express()
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-}
+app.use(express.static('client/build'));
 
 const httpServer = createServer(app);
 httpServer.listen(PORT);
-const io = new Server(httpServer)
+const io = new Server(httpServer);
+
+const __dirname = path.resolve("");
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,"/client/build/index.html"))
+})
 
 io.on('connection',(socket)=>{
-    console.log("new connection is added");
     socket.on("get-document",async(documentId)=>{
         const document = await getDocument(documentId);
         socket.join(documentId);
@@ -37,3 +38,4 @@ io.on('connection',(socket)=>{
         })
     })
 })
+console.log("server is running on port ", PORT);
